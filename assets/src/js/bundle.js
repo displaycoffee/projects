ff01ecd96123d6ac538c98e351dda3d825e03e6c
displaycoffee <adria.m.murphy@gmail.com>
2023-09-13 17:45:50 -0600

    update casing

--- assets/src/js/bundle.js
+++ assets/src/js/bundle.js
@@ -1,10 +1,10 @@
-/* react */
+/* React */
 import { createRoot } from 'react-dom/client';
 
-/* local components */
+/* Local components */
 import { Index } from './pages/Index';
 
-/* create root into app entry point */
+/* Create root into app entry point */
 const rootElement = document.getElementById('root');
 const rootTarget = createRoot(rootElement);
 rootTarget.render(<Index />);
