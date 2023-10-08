8a775f5b41a00554e978cfa44dc5638b1d467a81
displaycoffee <adria.m.murphy@gmail.com>
2023-10-08 16:23:26 -0600

    update readme

--- vite.config.js
+++ vite.config.js
@@ -7,21 +7,22 @@ export default defineConfig({
 		host: 'localhost',
 		port: 3000,
 	},
-	hmr: { clientPort: 3000 },
 	root: 'src',
 	build: {
 		outDir: '../dist',
 		rollupOptions: {
 			output: {
 				assetFileNames: (file) => {
-					console.log('files', import.meta.url, file.name);
-					if (file.name == 'index.css') {
-						return `assets/[ext]/styles.[ext]`;
+					if (file.name.includes('.css')) {
+						const suffix = file.name == 'index.css' ? `.css` : `.${file.name.toLowerCase()}`;
+						return `assets/[ext]/styles${suffix}`;
 					} else {
 						return `assets/[ext]/[name].[ext]`;
 					}
 				},
-				chunkFileNames: 'assets/[chunks]/[name].[hash].js',
+				chunkFileNames: (file) => {
+					return `assets/js/app.${file.name.toLowerCase()}.js`;
+				},
 				entryFileNames: 'assets/js/app.js',
 			},
 		},
